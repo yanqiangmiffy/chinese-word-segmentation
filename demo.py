5bd95690ec95bdf00567ae8db038d2bd7503bd4f
yanqiangmiffy <1185918903@qq.com>
2018-08-29 16:33:09 +0800

    08.29

--- demo.py
+++ demo.py
@@ -1,14 +1,37 @@
 str='偶尔  有  老乡  拥  上来  想  看  “  大官  ”  ，  立即  会  遭到  “  闪开  ！'
 # str='“  种菜  ，  也  有  烦恼  ，  那  是  累  的  时候  ；  另外  ，  大  棚  菜  在  降价  。'
 
-def clean(s):
-    if '“' not in s:  # 句子中间的引号不应去掉
-        return s.replace(' ”', '')
-    elif '”' not in s:
-        return s.replace('“ ', '')
-    elif '‘' not in s:
-        return s.replace(' ’', '')
-    elif '’' not in s:
-        return s.replace('‘ ', '')
-    else:
-        return s
+# def clean(s):
+#     if '“' not in s:  # 句子中间的引号不应去掉
+#         return s.replace(' ”', '')
+#     elif '”' not in s:
+#         return s.replace('“ ', '')
+#     elif '‘' not in s:
+#         return s.replace(' ’', '')
+#     elif '’' not in s:
+#         return s.replace('‘ ', '')
+#     else:
+#         return s
+
+# from itertools import chain
+# s1=['i','l','o','v','e']
+# s2=['y','o','u']
+# s=[s1,s2]
+#
+# all_letters=chain(*s)
+# print(all_letters,type(all_letters))
+# print(list(all_letters))
+
+
+# words=["我","北京","天安门"]
+# word2id={word:index for index,word in enumerate(words)}
+# id2word={index:word for index,word in enumerate(words)}
+# print(word2id)
+# print(id2word)
+#
+# import pandas as pd
+# ids=range(len(words))
+# word2id=pd.Series(ids,index=words).to_dict()
+# id2word=pd.Series(words,index=ids).to_dict()
+# print(word2id)
+# print(id2word)
--- utils.py
+++ utils.py
@@ -21,35 +21,17 @@ class DataHelper():
         self.y=None
         self.model_path=model_path # 存储路径
 
-
-    def process_data(self,filename):
+    def read_data(self, filename):
         """
-        预处理训练数据
-        :param filename:
-        :return:
-        """
-        texts=self.read_data(filename) # 从训练文件读取所有行
-        texts="".join(map(self.clean,texts)) # clean
-        sentences=re.split('[，。！？、‘’“”]/[bems]',texts) # 切分句子
-
-        lines=list()
-        labels=list()
-        for sentence in sentences:
-            result=1
-
-
-
-    def read_data(self,filename):
-        """
-        读取数据
+        读取数据  （process_data函数中将会用到）
         :param filename:
         :return: 返回句子列表
         """
-        return [line.strip() for line in codecs.open(filename,'r','utf-8').readlines()] # strip()
+        return [line.strip() for line in codecs.open(filename, 'r', 'utf-8').readlines()]  # strip()
 
-    def clean(self,text):
+    def clean(self, text):
         """
-        清洗文本 去掉不必要的引号
+        清洗文本 去掉不必要的引号 （process_data函数中将会用到）
         :param text:
         :return:
         """
@@ -70,8 +52,69 @@ class DataHelper():
         :param sentence:
         :return:
         """
+        words_tags=re.findall('(.)/(.)',sentence)
+        if words_tags:
+            words_tags=np.asarray(words_tags)
+            words=words_tags[:,0]
+            tags=words_tags[:,1]
+            return words,tags
+
+    def transform(self,df_data):
+        """
+        word、tags与id之间的转换
+        :param df_data:
+        :return:
+        """
+        # 1. 用chain(*lists)函数将多个list拼接起来
+        all_words=list(chain(*df_data['words'].values))
+
+        # 2. 统计所有 word
+        sr_words=pd.Series(all_words)
+        sr_words=sr_words.value_counts()
+        set_words=sr_words.index
+        self.vocab_size=len(set_words)
+        set_ids=range(1,len(set_words)+1) # 0 为填充值
+        tags=['x','s','b','m','e']
+        tags_ids=range(len(tags))
+
+        # 3. 构建 words 和 tags 都转为数值 id 的映射（使用 Series 比 dict 更加方便）
+        word2id=pd.Series(set_ids,index=set_words)
+        id2word=pd.Series(set_words,index=set_ids)
+        tag2id=pd.Series(tags_ids,index=tags)
+        id2tag=pd.Series(tags,index=tags_ids)
+        self.word2id=word2id
+        self.id2word=id2word
+        self.tag2id=tag2id
+        self.id2tag=id2tag
+
+    def process_data(self,filename):
+        """
+        预处理训练数据
+        :param filename:
+        :return:
+        """
+        texts=self.read_data(filename) # 从训练文件读取所有行
+        texts="".join(map(self.clean,texts)) # clean
+        sentences=re.split('[，。！？、‘’“”]/[bems]',texts) # 切分句子
+        datas =list()
+        labels=list()
+
+        for sentence in tqdm(sentences):
+            result=self.get_xy(sentence)
+            if result:
+                datas.append(result[0])
+                labels.append(result[1])
+
+        df_data=pd.DataFrame({'words':datas,'labels':labels},index=range(len(datas)))
+        # 句子长度
+        df_data['sentence_len']=df_data['words'].apply(lambda words:len(words))
+
+
+
+
+
+
 
 if __name__ == '__main__':
     data=DataHelper('data')
-    lines=data.read_data('data/msr_train.txt')
-    print(lines)
+    data.process_data('data/msr_train.txt')
