c473e57c4b2f770a7442e7f83548bf5ef568fc6d
yanqiangmiffy <1185918903@qq.com>
2018-08-30 11:17:11 +0800

    utils

--- utils.py
+++ utils.py
@@ -170,7 +170,7 @@ class DataHelper():
         df_data['sentence_len']=df_data['words'].apply(lambda words:len(words))
         self.transform(df_data)
         df_data['X']=df_data['words'].apply(self.X_padding)
-        df_data['y']=df_data['tags'].apply(self.y_padding())
+        df_data['y']=df_data['labels'].apply(self.y_padding)
         self.df_data=df_data
         X=np.asarray(list(df_data['X'].values))
         y=np.asarray(list(df_data['y'].values))
@@ -197,6 +197,70 @@ class DataHelper():
         ids = np.asarray(ids).reshape([-1, max_len])
         return ids
 
-if __name__ == '__main__':
-    data=DataHelper('data')
-    data.process_data('data/msr_train.txt')
+class BatchGenerator(object):
+
+    """ Construct a Data generator. The input X, y should be ndarray or list like type.
+
+    Example:
+        Data_train = BatchGenerator(X=X_train_all, y=y_train_all, shuffle=False)
+        Data_test = BatchGenerator(X=X_test_all, y=y_test_all, shuffle=False)
+        X = Data_train.X
+        y = Data_train.y
+        or:
+        X_batch, y_batch = Data_train.next_batch(batch_size)
+     """
+
+    def __init__(self,X,y,shuffle=False):
+        if type(X)!=np.ndarray:
+            X=np.asarray(X)
+        if type(y)!=np.ndarray:
+            y=np.asarray(y)
+
+        self._X=X
+        self._y=y
+        self._epochs_completed=0
+        self._index_in_epoch=0
+        self._number_examples=self._X.shape[0]
+        self._shuffle=shuffle
+        if self._shuffle:
+            new_index=np.random.permutation(self._number_examples)
+            self._X=self._X[new_index]
+            self._y=self._y[new_index]
+
+    @property
+    def X(self):
+        return self._X
+
+    @property
+    def y(self):
+        return self._y
+
+    @property
+    def num_examples(self):
+        return self._number_examples
+
+    @property
+    def epochs_completed(self):
+        return self._epochs_completed
+
+    def next_batch(self,batch_size):
+        """ Return the next 'batch_size' examples from this data set."""
+        start=self._index_in_epoch
+        self._index_in_epoch=+batch_size
+
+        if self._index_in_epoch > self._number_examples:
+            # finished epoch
+            self._epochs_completed += 1
+            # Shuffle the data
+            if self._shuffle:
+                new_index = np.random.permutation(self._number_examples)
+                self._X = self._X[new_index]
+                self._y = self._y[new_index]
+            start = 0
+            self._index_in_epoch = batch_size
+            assert batch_size <= self._number_examples
+        end = self._index_in_epoch
+        return self._X[start:end], self._y[start:end]
+# if __name__ == '__main__':
+#     data=DataHelper('model/vocab_model.pkl')
+#     data.process_data('data/msr_train.txt')
